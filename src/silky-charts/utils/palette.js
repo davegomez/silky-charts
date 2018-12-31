20d2343a2961546edbd35693267ef93f1ab1fd41
David Gomez <dg.dave@gmail.com>
2018-12-31 09:47:14 -0500

    Update theme system

---
+++ src/silky-charts/utils/palette.js
@@ -0,0 +1,53 @@
+export const white = 'rgb(255, 255, 255)'; // #FFFFFF
+export const black = 'rgb(33, 33, 33)'; // #212121
+export const grid = 'rgb(220, 220, 220)'; // #DCDCDC
+
+export const theme = {
+  red: {
+    base: 'rgb(178, 25, 17)', // #B21911
+    hover: 'rgb(255, 59, 48)', // #FF3B30
+    ref: 'rgb(48, 255, 128)', // #30FF80
+  },
+  green: {
+    base: 'rgb(35, 140, 53)', // #238C35
+    hover: 'rgb(76, 217, 100)', // #4CD964
+    ref: 'rgb(217, 76, 104)', // #D94C68
+  },
+  blue: {
+    base: 'rgb(0, 85, 178)', // #0055B2
+    hover: 'rgb(0, 122, 255)', // #007AFF
+    ref: 'rgb(255, 162, 0)', // #FFA200
+  },
+  orange: {
+    base: 'rgb(178, 104, 0)', // #B26800
+    hover: 'rgb(255, 149, 0)', // #FF9500
+    ref: 'rgb(0, 156, 255)', // #009CFF
+  },
+  purple: {
+    base: 'rgb(43, 42, 137)', // #2B2A89
+    hover: 'rgb(88, 86, 214)', // #5856D6
+    ref: 'rgb(214, 198, 86)', // #D6C056
+  },
+  yellow: {
+    base: 'rgb(178, 143, 0)', // #B28F00
+    hover: 'rgb(255, 204, 0)', // #FFCC00
+    ref: 'rgb(0, 12, 255)', // #000CFF
+  },
+  tealBlue: {
+    base: 'rgb(45, 133, 173)', // #2D85AD
+    hover: 'rgb(90, 200, 250)', // #5AC8FA
+    ref: 'rgb(250, 177, 90)', // #FAB15A
+  },
+  pink: {
+    base: 'rgb(78, 16, 47)', // #B2102F
+    hover: 'rgb(255, 45, 85)', // #FF2D55
+    ref: 'rgb(45, 255, 82)', // #2DFF52
+  },
+};
+
+export default {
+  white,
+  black,
+  grid,
+  theme,
+};
