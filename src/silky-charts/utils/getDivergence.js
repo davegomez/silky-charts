16a8632be74f32a2fe70bbc7fe4910f389f64b1d
David Gomez <dg.dave@gmail.com>
2019-01-17 10:05:39 -0500

    Rename or remove legacy modules

--- src/silky-charts/utils/getDivergence.js
+++
@@ -1,4 +0,0 @@
-export default (value, currentValue) => {
-  const divergence = value - currentValue;
-  return divergence > 0 ? `+${divergence}` : divergence;
-};
--- src/silky-charts/utils/getLineDataForKeys.js
+++
@@ -1,4 +0,0 @@
-export default (keys, data) =>
-  keys.map(key =>
-    data.map(datum => ({ name: datum.name, value: datum[key], key }))
-  );
