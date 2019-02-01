3e9a39cbd39b4d5e29f6522db153bbc339659907
David Gomez <dg.dave@gmail.com>
2019-02-01 13:42:08 -0500

    Remove old distribution files

--- cjs/index.js
+++
@@ -1,2 +0,0 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),require("./chunk-996be02a.js"),require("react"),require("styled-components"),require("react-dom");var Bar=require("./Bar.js"),BarLine=require("./BarLine.js"),StackedArea=require("./StackedArea.js");exports.Bar=Bar.default,exports.BarLine=BarLine.default,exports.StackedArea=StackedArea.default;
-//# sourceMappingURL=index.js.map
--- esm/index.js
+++
@@ -1,2 +0,0 @@
-import"./chunk-501b9e58.js";import"react";import"styled-components";import"react-dom";export{default as Bar}from"./Bar.js";export{default as BarLine}from"./BarLine.js";export{default as StackedArea}from"./StackedArea.js";
-//# sourceMappingURL=index.js.map
