31769c0eae0ec775b753fa50f7b5cc44a7badb18
David Gomez <dg.dave@gmail.com>
2019-02-02 07:09:41 -0500

    Test new bundling configuration

--- rollup.config.js
+++ rollup.config.js
@@ -1,68 +1,81 @@
+import { readdirSync } from 'fs';
+import path from 'path';
 import babel from 'rollup-plugin-babel';
-import resolve from 'rollup-plugin-node-resolve';
-import replace from 'rollup-plugin-replace';
 import commonjs from 'rollup-plugin-commonjs';
+import replace from 'rollup-plugin-replace';
+import resolve from 'rollup-plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
-import ignore from 'rollup-plugin-ignore';
 import pkg from './package.json';
+
+const CODES = [
+  'THIS_IS_UNDEFINED',
+  'MISSING_GLOBAL_NAME',
+  'CIRCULAR_DEPENDENCY',
+];
+
+const EXTENSIONS = ['.ts', '.tsx', '.js', '.jsx', '.json'];
+const extensions = ['.ts', '.tsx', '.js', '.jsx', '.json'];
 
 const external = Object.keys(pkg.peerDependencies || {});
 const allExternal = [...external, Object.keys(pkg.dependencies || {})];
-const extensions = ['.ts', '.tsx', '.js', '.jsx', '.json'];
 
-const createCommonPlugins = () => [
+const getChunks = URI =>
+  readdirSync(path.resolve(URI))
+    .filter(x => x.includes('.js'))
+    .reduce((a, c) => ({ ...a, [c.replace('.js', '')]: `src/${c}` }), {});
+
+const discardWarning = warning => {
+  if (CODES.includes(warning.code)) {
+    return;
+  }
+
+  console.error(warning);
+};
+
+const env = process.env.NODE_ENV;
+
+const commonPlugins = () => [
   babel({
-    extensions,
+    extensions: EXTENSIONS,
     exclude: 'node_modules/**',
   }),
   commonjs({
     include: /node_modules/,
   }),
+  replace({ 'process.env.NODE_ENV': JSON.stringify(env) }),
+  resolve({
+    extensions,
+    preferBuiltins: false,
+  }),
 ];
 
-const main = {
-  input: 'src/index.js',
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
+export default [
+  {
+    onwarn: discardWarning,
+    input: 'src/index.js',
+    output: {
+      esModule: false,
+      file: pkg.unpkg,
+      format: 'umd',
+      name: 'silkyCharts',
       exports: 'named',
+      globals: {
+        react: 'React',
+        'react-dom': 'ReactDOM',
+        'styled-components': 'styled',
+      },
     },
-    {
-      file: pkg.module,
-      format: 'es',
-    },
-  ],
-  external: allExternal,
-  plugins: [...createCommonPlugins(), resolve({ extensions })],
-};
-
-const unpkg = {
-  input: 'src/index.js',
-  output: {
-    name: pkg.name,
-    file: pkg.unpkg,
-    format: 'umd',
-    exports: 'named',
-    globals: {
-      react: 'React',
-      'react-dom': 'ReactDOM',
-      'styled-components': 'styled',
-    },
+    external: allExternal,
+    plugins: [...commonPlugins(), env === 'production' && terser()],
   },
-  external,
-  plugins: [
-    ...createCommonPlugins(),
-    ignore(['stream']),
-    terser(),
-    replace({
-      'process.env.NODE_ENV': JSON.stringify('production'),
-    }),
-    resolve({
-      extensions,
-      preferBuiltins: false,
-    }),
-  ],
-};
-
-export default [main, unpkg];
+  {
+    onwarn: discardWarning,
+    input: getChunks('src'),
+    output: [
+      { dir: 'esm', format: 'esm', sourcemap: true },
+      { dir: 'cjs', format: 'cjs', exports: 'named', sourcemap: true },
+    ],
+    external: allExternal,
+    plugins: commonPlugins(),
+  },
+];
