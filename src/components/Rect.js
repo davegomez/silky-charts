2c054750b91bc713d9f36dace4e2eaaf2927b263
David Gomez <dg.dave@gmail.com>
2019-02-11 13:57:21 -0500

    Fix SVG rect element rendering in Firefox

--- src/components/Rect.js
+++ src/components/Rect.js
@@ -1,14 +1,16 @@
 import styled from 'styled-components';
 import { getHoverColor } from '../utils';
 
-const Rect = styled.rect.attrs(({ chart, position: { x, y } }) => ({
-  x,
-  y,
-  className: chart,
-}))`
+const Rect = styled.rect.attrs(
+  ({ chart, position: { x, y }, size: { height, width } }) => ({
+    className: chart,
+    height,
+    width,
+    x,
+    y,
+  })
+)`
   fill: ${({ fillColor }) => fillColor};
-  height: ${({ size }) => size.height}px;
-  width: ${({ size }) => size.width}px;
 
   &:hover {
     fill: ${({ fillColor }) => getHoverColor(fillColor)};
