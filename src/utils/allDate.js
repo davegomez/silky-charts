39b8151116b99dacaeb0d4b2c6af4554150c01fd
David Gomez <dg.dave@gmail.com>
2019-02-12 13:36:31 -0500

    Refactor date validation to replace date-fns

--- src/utils/allDate.js
+++ src/utils/allDate.js
@@ -1,5 +1,15 @@
 import all from 'ramda/src/all';
 import compose from 'ramda/src/compose';
-import isISODate from './isISODate';
+import equals from 'ramda/src/equals';
+import or from 'ramda/src/or';
+import type from 'ramda/src/type';
+import isValidDate from './isValidDate';
 
-export default all(isISODate);
+const isString = equals('String');
+const isDate = equals('Date');
+const isStringOrDate = x => or(isString(type(x)), isDate(type(x)));
+
+const allStringOrDate = all(isStringOrDate);
+const allValidDate = all(isValidDate);
+
+export default dates => allStringOrDate(dates) && allValidDate(dates);
