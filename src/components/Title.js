97d432761db1c987a88828adc8b2062aedce0d76
David Gomez <dg.dave@gmail.com>
2019-02-12 04:22:15 -0500

    Adjust chart title position

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Title = styled.text.attrs(({ height, margin, width }) => ({
   className: `chart-title`,
   x: width / 2,
-  y: 0 - margin.top / 1.5,
+  y: 0 - margin.top / 2,
 }))`
   font-size: 24px;
   text-anchor: middle;
