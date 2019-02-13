250f093587ce47bdeaef55cb50f0f18f4cc0f4d1
David Gomez <dg.dave@gmail.com>
2019-02-13 12:54:28 -0500

    Reposition labels based on margin values

--- src/components/Label.js
+++ src/components/Label.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Label = styled.text.attrs(({ axis, margin, width, height }) => ({
   className: `${axis}-axis-label`,
   x: axis === 'x' ? width / 2 : 0 - height / 2,
-  y: axis === 'x' ? height + margin.bottom / 1.5 : 0 - margin.left / 1.5,
+  y: axis === 'x' ? height + margin.bottom - 30 : 34 - margin.left,
 }))`
   text-anchor: middle;
   transform: ${({ axis }) => axis === 'y' && 'rotate(-90deg)'};
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 
-const Title = styled.text.attrs(({ height, margin, width }) => ({
+const Title = styled.text.attrs(({ margin, width }) => ({
   className: `chart-title`,
   x: width / 2,
-  y: 0 - margin.top / 2,
+  y: 50 - margin.top,
 }))`
   font-size: 1.5em;
   text-anchor: middle;
