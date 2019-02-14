a9facbb6d8fa31ce26e589c0bf9da4c4a9150a50
David Gomez <dg.dave@gmail.com>
2019-02-14 15:49:13 -0500

    Fill the line scatterplots and move up the tooltip

--- src/components/Circle.js
+++ src/components/Circle.js
@@ -1,11 +1,7 @@
 import styled from 'styled-components';
-import { white } from '../utils/palette';
-import { LINE_STROKE_WIDTH } from '../utils/constants';
 
 const Circle = styled.circle.attrs(({ chart }) => ({ className: chart }))`
-  fill: ${white};
-  stroke: ${({ strokeColor }) => strokeColor};
-  stroke-width: ${LINE_STROKE_WIDTH};
+  fill: ${({ color }) => color};
 `;
 
 export default Circle;
