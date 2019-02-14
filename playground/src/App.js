21ee83b8ec1d90957405d75767607405335f32fd
David Gomez <dg.dave@gmail.com>
2019-02-14 05:00:13 -0500

    Update playground to include testing chart and data

--- playground/src/App.js
+++ playground/src/App.js
@@ -1,28 +1,43 @@
+/* eslint-disable */
+
 import React from 'react';
 import styled from 'styled-components';
+import { Bar, BarLine, StackedArea } from 'silky-charts';
+import { letters, aapl, webServers, fruits } from './data';
+import Container from './components/Container';
+import ChartContainer from './components/ChartContainer';
 import 'normalize.css';
 import './styles.css';
-
-const Container = styled.div`
-  align-items: center;
-  background-color: rgba(0, 122, 255, 0.1);
-  display: flex;
-  justify-content: center;
-  height: 100vh;
-  width: 100vw;
-`;
-
-const ChartContainer = styled.div`
-  background-color: rgb(255, 255, 255);
-  width: 80%;
-`;
 
 const App = () => (
   <Container>
     <ChartContainer>
-      {
-        // content
-      }
+      <BarLine
+        // aspectRatio="4:3"
+        data={fruits}
+        // dateFormat="%b %d, %Y"
+        grid
+        // height="300"
+        lineSeries={['Sold', 'Lost']}
+        margin={{ top: 100, right: 100, bottom: 100, left: 100 }}
+        // onClick={() => {}}
+        // onMouseEnter={() => {}}
+        // onMouseLeave={() => {}}
+        // padding={2}
+        responsive
+        // theme="curiousBlue"
+        sourceLabel="Unknown"
+        stackedSeries={['Pear', 'Mango', 'Papaya']}
+        title="Placeholder Chart Title"
+        tooltip
+        // width={500}
+        xAxisChartLabel="X axis label"
+        // xAxisLabelRotation
+        // xAxisLabelRotationValue={-65}
+        // xAxisTicks={10}
+        yAxisChartLabel="Y axis label"
+        // yAxisTicks={10}
+      />
     </ChartContainer>
   </Container>
 );
---
+++ playground/src/components/Container.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  align-items: center;
+  background-color: rgba(0, 122, 255, 0.1);
+  display: flex;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+`;
+
+export default Container;
