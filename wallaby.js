b63946652b24604c2ac8395481c1c13b5b8073b0
David Gomez <dg.dave@gmail.com>
2019-02-16 19:01:37 -0500

    Update Wallaby configuration

--- wallaby.js
+++ wallaby.js
@@ -1,14 +1,4 @@
 module.exports = function(wallaby) {
-  // Babel, jest-cli and some other modules may be located under
-  // react-scripts/node_modules, so need to let node.js know about it
-  var path = require('path');
-  process.env.NODE_PATH +=
-    path.delimiter +
-    path.join(__dirname, 'node_modules') +
-    path.delimiter +
-    path.join(__dirname, 'node_modules/react-scripts/node_modules');
-  require('module').Module._initPaths();
-
   return {
     files: [
       'src/**/*.+(js|jsx|json|snap|css|less|sass|scss|jpg|jpeg|gif|png|svg)',
@@ -22,27 +12,14 @@ module.exports = function(wallaby) {
       runner: 'node',
     },
 
-    compilers: {
-      '**/*.js?(x)': wallaby.compilers.babel({
-        presets: ['react-app'],
-      }),
-    },
-
     hints: {
       ignoreCoverageForFile: /ignore file coverage/,
     },
 
-    setup: wallaby => {
-      const jestConfig = require('react-scripts/scripts/utils/createJestConfig')(
-        p => require.resolve('react-scripts/' + p)
-      );
-      Object.keys(jestConfig.transform || {}).forEach(
-        k => ~k.indexOf('^.+\\.(js|jsx') && void delete jestConfig.transform[k]
-      );
-      delete jestConfig.testEnvironment;
-      wallaby.testFramework.configure(jestConfig);
-    },
-
     testFramework: 'jest',
+
+    compilers: {
+      '**/*.js': wallaby.compilers.babel(),
+    },
   };
 };
