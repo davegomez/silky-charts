7232d5ab71b7f1d0d23e551051e35d0cd342ce2a
David Gomez <dg.dave@gmail.com>
2019-02-23 22:26:31 -0500

    Add utility documentation

--- src/utils/bySeries.js
+++ src/utils/bySeries.js
@@ -3,6 +3,11 @@ import groupBy from 'ramda/src/groupBy';
 import prop from 'ramda/src/prop';
 import toPairs from 'ramda/src/toPairs';
 
+/**
+ * The data must be ordered by series before the area is created.
+ *
+ * @returns {Function} Ordering function
+ */
 export default compose(
   toPairs,
   groupBy(prop('series'))
