1a40b3c22bb2a1a1abc9dcb64e23b56ae6ee2e34
David Gomez <dg.dave@gmail.com>
2019-02-23 22:27:06 -0500

    Add utility documentation

--- src/utils/extent.js
+++ src/utils/extent.js
@@ -1,4 +1,10 @@
 import max from 'ramda/src/max';
 import min from 'ramda/src/min';
 
+/**
+ * Find the minimum and maximum value in a given list.
+ *
+ * @param {Array} xs List of values.
+ * @returns {Array} Minimum and maximum value in the list.
+ */
 export default xs => [xs.reduce(min), xs.reduce(max)];
