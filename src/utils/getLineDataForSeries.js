55edd48ca2696e204b0bf8f1c257690583feb00e
David Gomez <dg.dave@gmail.com>
2019-02-24 18:59:50 -0500

    Add utility documentation

--- src/utils/getLineDataForSeries.js
+++ src/utils/getLineDataForSeries.js
@@ -1,2 +1,10 @@
+/**
+ * Filter the data for the series selected to be represented as lines in the
+ * chart.
+ *
+ * @param {Array} series List of series to filter form the chart data.
+ * @param {Array} data Chart data list.
+ * @returns {Array} Data to use as lines in the chart.
+ */
 export default (series, data) =>
   series.map(x => data.filter(datum => datum.series === x));
