569854697605e1e89ae9b5af5587c8f3d2e15358
David Gomez <dg.dave@gmail.com>
2019-02-24 18:59:40 -0500

    Add utility documentation

--- src/utils/getSize.js
+++ src/utils/getSize.js
@@ -1,5 +1,14 @@
 import { WIDTH } from './constants';
 
+/**
+ * Calculate the size of the chart using aspect-ratio, margins, and parent size.
+ *
+ * @param {Number} w1 Chart's width.
+ * @param {Number} h1 Chart's height.
+ * @param {Object} h1 Chart's margin.
+ * @param {String} r Chart's aspect-ratio.
+ * @returns {Object} Chart's width and height.
+ */
 export default (w1, h1, { top, right, bottom, left }, r) => {
   const [r1, r2] = r.split(':');
   const w2 = w1 || WIDTH;
