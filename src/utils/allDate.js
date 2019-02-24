61ad52ccded71e9613fa3f1142f46c00eee6c2c2
David Gomez <dg.dave@gmail.com>
2019-02-23 22:25:47 -0500

    Add utility documentation

--- src/utils/allDate.js
+++ src/utils/allDate.js
@@ -11,4 +11,10 @@ const isStringOrDate = x => or(isString(type(x)), isDate(type(x)));
 const allStringOrDate = all(isStringOrDate);
 const allValidDate = all(isValidDate);
 
+/**
+ * The values in the name field can be regular strings or ISO string dates.
+ *
+ * @param {[String]} dates List of strings to check.
+ * @returns {Boolean} True if all the values are Dates.
+ */
 export default dates => allStringOrDate(dates) && allValidDate(dates);
