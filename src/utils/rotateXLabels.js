e39b961a17eaf6c284b983930a15c9bbf8464ad8
David Gomez <dg.dave@gmail.com>
2019-02-24 18:59:22 -0500

    Add utility documentation

--- src/utils/rotateXLabels.js
+++ src/utils/rotateXLabels.js
@@ -1,12 +1,12 @@
 import { selectAll as d3SelectAll } from 'd3-selection';
 
 /**
- * Rotate the X axis labels to given degrees
+ * Rotate the X axis labels to a given degrees.
  *
- * @param {String} id Unique chart id
- * @param {Number} deg Degrees to rotate
+ * @param {String} id Unique chart ID.
+ * @param {Number} deg Degrees to rotate.
+ * @returns {Void}
  */
-
 export default (id, deg) => {
   const isNegative = deg < 0;
   d3SelectAll(`#${id} .axis-x .tick text`)
