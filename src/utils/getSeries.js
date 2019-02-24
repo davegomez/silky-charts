d7561f3fa7fe472d37d89618aae4d83d875228e8
David Gomez <dg.dave@gmail.com>
2019-02-24 18:59:45 -0500

    Add utility documentation

--- src/utils/getSeries.js
+++ src/utils/getSeries.js
@@ -3,6 +3,13 @@ import map from 'ramda/src/map';
 import prop from 'ramda/src/prop';
 import uniq from 'ramda/src/uniq';
 
+/**
+ * Get the list of series in the chart data.
+ *
+ * @param {Array} _ Chart data.
+ * @returns {Function} Ordering function that returns a list of unique series
+ * names.
+ */
 export default compose(
   uniq,
   map(prop('series'))
