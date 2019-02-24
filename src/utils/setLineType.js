18c2094ca9317ebe855916f362e6f7fa3867bf45
David Gomez <dg.dave@gmail.com>
2019-02-24 18:59:17 -0500

    Add utility documentation

--- src/utils/setLineType.js
+++ src/utils/setLineType.js
@@ -3,6 +3,13 @@ import equals from 'ramda/src/equals';
 import T from 'ramda/src/T';
 import { LINE_TYPES } from './constants';
 
+/**
+ * Apply the option to the corresponding line type if exist.
+ *
+ * @param {String} type Line type.
+ * @param {Number} option Option value.
+ * @returns {Function} Line type setter.
+ */
 export default (type, option = null) =>
   cond([
     [equals('curveBundle'), () => LINE_TYPES[type].beta(option)],
