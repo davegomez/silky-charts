a06d2ab2a3fd8818fa3eb8fa946b67fee1e91928
David Gomez <dg.dave@gmail.com>
2019-02-24 18:59:12 -0500

    Add utility documentation

--- src/utils/setupData.js
+++ src/utils/setupData.js
@@ -1,5 +1,14 @@
 import { allDate } from './';
 
+/**
+ * Validates if the values in the data's name fields are valid dates and if is
+ * the case transfor this dates into instances of Date before returning a tuple
+ * with the validation result and the new data list.
+ *
+ * @param {Array} d Chart's data.
+ * @returns {Array} Tupple containing the dates validation result and the
+ * transformed new data list.
+ */
 export default d => {
   const isDates = allDate(d.map(({ name }) => name));
   const data = isDates ? d.map(x => ({ ...x, name: new Date(x.name) })) : d;
