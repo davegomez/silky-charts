78274f5774aed04f46c7ed62885744481346f247
David Gomez <dg.dave@gmail.com>
2019-02-23 22:26:01 -0500

    Add utility documentation

--- src/utils/appendStackedValues.js
+++ src/utils/appendStackedValues.js
@@ -1,5 +1,13 @@
 let idx = 0;
 
+/**
+ * The "stacked" charts use these values to generate both instances of the Y
+ * axis.
+ *
+ * @param {Object} stack D3 stack object.
+ * @param {Array} data Chart data list.
+ * @returns {Array} The modified chart data list.
+ */
 export default (stack, data) => {
   stack.forEach(values => {
     data.forEach(datum => {
