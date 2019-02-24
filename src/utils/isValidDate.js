60d1e16cc4c13ee4865507411fac49b537cd9166
David Gomez <dg.dave@gmail.com>
2019-02-24 18:59:27 -0500

    Add utility documentation

--- src/utils/isValidDate.js
+++ src/utils/isValidDate.js
@@ -3,6 +3,13 @@ import compose from 'ramda/src/compose';
 
 const isNotNaN = complement(isNaN);
 
+/**
+ * Validate if the string passed is a valid ISO string date.
+ *
+ * @param {String} _ ISO string date to validate.
+ * @returns {Function} Validation function that returns true if the string is a
+ * valid date.
+ */
 export default compose(
   isNotNaN,
   Date.parse
