e6053b4b2c4dec8ec1677efe4a66e1b98091ac0a
David Gomez <dg.dave@gmail.com>
2019-02-23 22:26:55 -0500

    Add utility documentation

--- src/utils/drawGrid.js
+++ src/utils/drawGrid.js
@@ -1,5 +1,15 @@
 import { axisBottom as d3AxisBottom, axisLeft as d3AxisLeft } from 'd3-axis';
 
+/**
+ * @param {Boolean} horizontal Is the chart vertical or horizontal.
+ * @param {Function} xScale D3 scale.
+ * @param {Number} height Chart's available height.
+ * @param {Function} yScale D3 scale.
+ * @param {Number} width Chart's available width.
+ * @param {Number} xAxisTicks Suggested value for the X axis ticks.
+ * @param {Number} yAxisTicks Suggested value for the Y axis ticks.
+ * @returns {Void}
+ */
 export default (
   horizontal,
   xScale,
