d80392583b646267262a6cdc6a0e4b231631aaf4
David Gomez <dg.dave@gmail.com>
2019-02-23 22:26:44 -0500

    Add utility documentation

--- src/utils/classify.js
+++ src/utils/classify.js
@@ -1,1 +1,7 @@
-export default str => str.replace(/ /g, '-').toLowerCase();
+/**
+ * Use the series value to create a class to identify the element.
+ *
+ * @param {String} series Series name.
+ * @returns {String} Classname.
+ */
+export default series => series.replace(/ /g, '-').toLowerCase();
