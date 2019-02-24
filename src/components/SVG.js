8c17ed05b10322c5691445e16f254ad225a387b5
David Gomez <dg.dave@gmail.com>
2019-02-23 22:25:07 -0500

    Add standard SVG properties to SVG element

--- src/components/SVG.js
+++ src/components/SVG.js
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 const SVG = styled.svg.attrs(({ identifier }) => ({
   id: identifier,
   className: 'silky-charts',
+  baseProfile: 'full',
+  xmlns: 'http://www.w3.org/2000/svg',
 }))`
   height: ${({ size }) => size.height}px;
   width: ${({ size }) => size.width}px;
