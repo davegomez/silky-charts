97f944b33f61a50049beac0b165ece7f8bf417ba
David Gomez <dg.dave@gmail.com>
2019-02-24 19:00:04 -0500

    Add utility documentation

--- src/utils/getBaseColor.js
+++ src/utils/getBaseColor.js
@@ -1,4 +1,10 @@
 import head from 'ramda/src/head';
 import { themes } from './palette';
 
+/**
+ * Use the first color in the color theme list to use it as base color.
+ *
+ * @param {Array} theme Color theme list
+ * @returns {String} Color
+ */
 export default theme => head(themes[theme]);
