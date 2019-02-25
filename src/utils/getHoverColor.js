c97726fc6637c1c484e5f0cfe8837ac01ccb6257
David Gomez <dg.dave@gmail.com>
2019-02-24 19:00:00 -0500

    Add utility documentation

--- src/utils/getHoverColor.js
+++ src/utils/getHoverColor.js
@@ -1,3 +1,11 @@
+/**
+ * You can generate a different color shade passing a second argument with the
+ * amount to apply in order to make it darker or lighter.
+ *
+ * @param {String} rgb RGB color.
+ * @param {Number} amt Amount to apply in order to make it darker or lighter.
+ * @returns {String} RGB color.
+ */
 export default (rgb = 'rgb(0, 0, 0)', amt = -20) =>
   `rgb(${rgb
     .split(/\D/)
